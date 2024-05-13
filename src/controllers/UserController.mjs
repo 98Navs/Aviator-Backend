1919b1e2c6eea112bf26badf49a3b92c2fcc70d4
Navdeep Ruhil <navdeep@scriza.in>
2024-05-13 10:47:58 +0530

    Service class removed

--- src/controllers/UserController.mjs
+++ src/controllers/UserController.mjs
@@ -1,11 +1,11 @@
 //scr/controllers/UserController.mjs
-import UserService from "../services/UserService.mjs";
+import UserRepository from "../repositories/UserRepository.mjs";
     
 class UserController {
     static async createUser(req, res) {
         try {
             const userData = req.body;
-            const user = await UserService.createUser(userData);
+            const user = await UserRepository.createUser(userData);
             res.status(201).json(user);
         } catch (error) {
             res.status(400).json({ error: error.message });
@@ -14,7 +14,7 @@ class UserController {
 
     static async getAllUsers(req, res) {
         try {
-            const users = await UserService.getAllUsers();
+            const users = await UserRepository.getAllUsers();
             res.json(users);
         } catch (error) {
             res.status(500).json({ error: error.message });
@@ -24,7 +24,7 @@ class UserController {
     static async getUserById(req, res) {
         try {
             const userId = req.params.id;
-            const user = await UserService.getUserById(userId);
+            const user = await UserRepository.getUserById(userId);
             res.json(user);
         } catch (error) {
             res.status(404).json({ error: error.message });
@@ -35,7 +35,7 @@ class UserController {
         try {
             const userId = req.params.id;
             const userData = req.body;
-            const user = await UserService.updateUserById(userId, userData);
+            const user = await UserRepository.updateUserById(userId, userData);
             res.json(user);
         } catch (error) {
             res.status(404).json({ error: error.message });
@@ -45,7 +45,7 @@ class UserController {
     static async deleteUserById(req, res) {
         try {
             const userId = req.params.id;
-            const user = await UserService.deleteUserById(userId);
+            const user = await UserRepository.deleteUserById(userId);
             res.json(user);
         } catch (error) {
             res.status(404).json({ error: error.message });
