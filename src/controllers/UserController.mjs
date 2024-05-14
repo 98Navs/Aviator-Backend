8d4be4a4d6ecd0a45c3ddc19edf4dfd702293cb5
Navdeep Ruhil <navdeep@scriza.in>
2024-05-14 12:41:38 +0530

    user

--- src/controllers/UserController.mjs
+++ src/controllers/UserController.mjs
@@ -1,7 +1,7 @@
 // src/controllers/UserController.mjs
 import bcrypt from 'bcrypt';
 import UserRepository from "../repositories/UserRepository.mjs";
-import GenerateSignature  from "../project_setup/Utils.mjs"
+import GenerateSignature from "../project_setup/Utils.mjs";
 
 class UserController {
     static async createUser(req, res) {
@@ -20,24 +20,19 @@ class UserController {
     static async signIn(req, res) {
         try {
             const { user, password } = req.body;
-
-            if (!user) return res.status(400).json({ error: 'Please provide email, userName, or mobile.' });
-
+            if (!user) return res.status(400).json({ error: 'Please provide email, userId, or mobile.' });
             let existingUser = null;
             if (typeof user === 'string') {
                 if (user.includes('@')) existingUser = await UserRepository.getUserByEmail(user.toLowerCase());
-                else existingUser = await UserRepository.getUserByUserName(user.toLowerCase());
-            } else if (!isNaN(user)) {
-                existingUser = await UserRepository.getUserByMobile(user);
+                else if (/^\d{10}$/.test(user)) existingUser = await UserRepository.getUserByMobile(parseInt(user, 10));
+                else if (!isNaN(user)) existingUser = await UserRepository.getUserByUserId(user);
             } else {
-                return res.status(400).json({ error: 'Invalid email, userName, or mobile provided.' });
+                return res.status(400).json({ error: 'Invalid email, userId, or mobile provided.' });
             }
-
             if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
-                return res.status(401).json({ error: 'Invalid password.' });
+                return res.status(401).json({ error: 'Invalid credentials.' });
             }
             const token = await GenerateSignature({ userId: existingUser.userId, email: existingUser.email, role: existingUser.role }, res);
-
             res.json({ message: 'Sign in successful!', user: { userId: existingUser.userId, email: existingUser.email, token } });
         } catch (error) {
             res.status(400).json({ error: error.message });
@@ -80,28 +75,27 @@ class UserController {
 
 async function validateUserData(userData) {
     const { userName, email, mobile, promoCode, password, userId, referenceCode } = userData;
-
     userData.userName = userName.toLowerCase();
     userData.email = email.toLowerCase();
-
     const requiredFields = ['userName', 'email', 'mobile', 'password'];
     const missingFields = requiredFields.filter(field => !userData[field] || (userData[field].trim && userData[field].trim() === ''));
-    if (missingFields.length > 0) {
-        return { error: `Missing or empty fields: ${missingFields.join(', ')}` };
-    }
-    const error = [
-        { field: 'userName', validator: (val) => val.length >= 4 && !/[^a-zA-Z0-9]/.test(val), message: 'Invalid username provided. Username must be at least 4 characters long and cannot contain special characters.' },
-        { field: 'email', validator: (val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val), message: 'Invalid email provided.' },
-        { field: 'mobile', validator: (val) => /^\d{10}$/.test(val), message: 'Invalid mobile number provided.' },
-        { field: 'password', validator: (val) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/.test(val), message: 'Invalid password. Must be at least 8 characters long, with at least one lowercase letter, one uppercase letter, one digit, and one special character' }
-    ].find(({ field, validator }) => !validator(userData[field]));
-    if (error) return { error: error.message };
-
-    if (await UserRepository.getUserByUserName(userData.userName)) return { error: 'Username is already taken.' };
-    if (await UserRepository.getUserByEmail(userData.email)) return { error: 'Email is already registered.' };
-    if (await UserRepository.getUserByMobile(mobile)) return { error: 'Mobile number is already registered.' };
-    if (await UserRepository.getUserByPromoCode(promoCode)) return { error: 'Promo code is already used.' };
-    if (await UserRepository.getUserByUserId(userId)) return { error: 'UserId code is already used.' };
+    if (missingFields.length > 0) return { error: `Missing or empty fields: ${missingFields.join(', ')}` };
+    const validators = {
+        userName: (val) => /^[a-zA-Z]+(?: [a-zA-Z]+)*$/.test(val) && val.length >= 4,
+        email: (val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val),
+        mobile: (val) => /^\d{10}$/.test(val),
+        password: (val) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/.test(val)
+    };
+    const errorField = Object.keys(validators).find(field => !validators[field](userData[field]));
+    if (errorField) return { error: validators[errorField].message };
+    const existingEmail = await UserRepository.getUserByEmail(userData.email);
+    if (existingEmail) return { error: 'Email is already registered.' };
+    const existingMobile = await UserRepository.getUserByMobile(mobile);
+    if (existingMobile) return { error: 'Mobile number is already registered.' };
+    const existingPromoCode = await UserRepository.getUserByPromoCode(promoCode);
+    if (existingPromoCode) return { error: 'Promo code is already used.' };
+    const existingUserId = await UserRepository.getUserByUserId(userId);
+    if (existingUserId) return { error: 'UserId code is already used.' };
     if (referenceCode) {
         const existingRefUser = await UserRepository.getUserByReferenceCode(referenceCode.toUpperCase());
         userData.referenceCode = existingRefUser ? referenceCode.toUpperCase() : 'admin';
