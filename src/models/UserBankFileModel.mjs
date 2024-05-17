5f08426a04e72348178bee5e7955ca468c97d111
thakurvinaysingh <vinay.singh5497@gmail.com>
2024-05-17 14:44:37 +0530

    Recharge

---
+++ src/models/UserBankFileModel.mjs
@@ -0,0 +1,30 @@
+//src/models/bankfile.js
+import { Schema, model } from 'mongoose';
+
+
+const userBankAccountSchema = new Schema({
+    bankName: {
+        type: String,
+        required: true
+    },
+    accountNumber: {
+        type: String,
+        required: true
+    },
+    accountHolderName: {
+        type: String,
+        required: true
+    },
+    ifscCode: {
+        type: String,
+        required: true
+    },
+    upiId: {
+        type: String,
+        required: true
+    }
+
+})
+const UserBankAccount = model('UserBankAccount', userBankAccountSchema);
+
+export default UserBankAccount;
