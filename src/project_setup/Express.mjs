481f256cc3a7b2f4d16100ea6223439c7376c8c7
Navdeep Ruhil <navdeep@scriza.in>
2024-11-18 16:08:55 +0530

    cors added

--- src/project_setup/Express.mjs
+++ src/project_setup/Express.mjs
@@ -12,7 +12,7 @@ export default async function setupExpressApp() {
     app.use(cookieParser());
     app.use(express.json({ limit: '1mb' }));
     app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-    app.use(cors({ credentials: true, origin: [ 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:5173', 'https://gamingadmin.vercel.app', 'http://localhost:1111' ] }));
+    app.use(cors({ credentials: true, origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:5173', 'https://gamingadmin.vercel.app', 'http://localhost:1111', 'https://mobile-admin-live.vercel.app' ] }));
     app.use(express.static('src/public'));
 
   //  monitor.exposeMetrics(app);
